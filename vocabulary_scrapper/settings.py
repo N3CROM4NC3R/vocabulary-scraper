9799de7cc30053817e1dca6927d15e31e1eeb5a4
N3CROM4NC3R <santi16648@gmail.com>
2022-07-30 16:56:12 -0400

    fix:deleting comments of the sqlite

--- vocabulary_scrapper/settings.py
+++ vocabulary_scrapper/settings.py
@@ -115,11 +115,6 @@ if 'RDS_HOSTNAME' in os.environ:
     }
 else:
     DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-
     'default': {
         'ENGINE':'django.db.backends.mysql',
         'NAME':'scrapper',
